7d9f6fbc73f7a4a4552d7059aa85603be43e8491
DeanLeong <63728899+DeanLeong@users.noreply.github.com>
2020-09-21 11:11:38 -0400

    Create app.js

---
+++ app.js
@@ -0,0 +1,11 @@
+const getOptions = async () => {
+  const url = 'http://ddragon.leagueoflegends.com/cdn/10.19.1/data/en_US/champion.json'
+  try {
+    const response = await axios.get(url)
+    console.log(response.data)
+  } catch (error) {
+    console.log(`Error: ${error}`)
+  }
+}
+
+getOptions()
